4bc641a3d19c9431f157d6a23ecc218d9fdc65d4
git geek <cout.1.endl@gmail.com>
2023-01-10 22:59:25 -0500

    Updates

---
+++ precache-manifest.c48befae194100b181e76e5b18df1632.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "abfee28fc5643a82937b",
+    "url": "/Dev-Portfolio/static/css/main.a0bb62d1.chunk.css"
+  },
+  {
+    "revision": "abfee28fc5643a82937b",
+    "url": "/Dev-Portfolio/static/js/main.abfee28f.chunk.js"
+  },
+  {
+    "revision": "4620a94911b6f48ca4d3",
+    "url": "/Dev-Portfolio/static/js/1.4620a949.chunk.js"
+  },
+  {
+    "revision": "c75c533bf3b178718a52",
+    "url": "/Dev-Portfolio/static/js/runtime~main.c75c533b.js"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/Dev-Portfolio/static/media/js.0cded3a3.png"
+  },
+  {
+    "revision": "22a7a15a52a0b154cd5d33573d99eef1",
+    "url": "/Dev-Portfolio/static/media/bootstrap.22a7a15a.png"
+  },
+  {
+    "revision": "07c28adf28e0592b46094aef81fcc655",
+    "url": "/Dev-Portfolio/static/media/mysql.07c28adf.png"
+  },
+  {
+    "revision": "4c0ae90bdde606cba342bc8b2d36302e",
+    "url": "/Dev-Portfolio/static/media/css.4c0ae90b.png"
+  },
+  {
+    "revision": "a3ce4d3d43df84bdb902dc9690aa8f20",
+    "url": "/Dev-Portfolio/static/media/react.a3ce4d3d.png"
+  },
+  {
+    "revision": "73342c25b9a70f33d2cdc5788846fff1",
+    "url": "/Dev-Portfolio/static/media/socket.io.73342c25.svg"
+  },
+  {
+    "revision": "671fd7afbe0996ed40ad20c591d7f836",
+    "url": "/Dev-Portfolio/static/media/Who's App.demo.671fd7af.gif"
+  },
+  {
+    "revision": "a8ab2f89a4514f27ea1dc2ee6bcc25d6",
+    "url": "/Dev-Portfolio/static/media/DrinksDemo.a8ab2f89.gif"
+  },
+  {
+    "revision": "d34424945407856a805e24f43afb46eb",
+    "url": "/Dev-Portfolio/static/media/express.d3442494.png"
+  },
+  {
+    "revision": "79aeb7676caa5d83f981717266815301",
+    "url": "/Dev-Portfolio/static/media/openWeather.79aeb767.png"
+  },
+  {
+    "revision": "9c9553413dab14c95628c705586176e5",
+    "url": "/Dev-Portfolio/static/media/jQ.9c955341.png"
+  },
+  {
+    "revision": "82bc5c48da78167f6320557f12dc3357",
+    "url": "/Dev-Portfolio/static/media/ttDemo.82bc5c48.gif"
+  },
+  {
+    "revision": "74a92a2e4ba4cf4092fbee241de8fee6",
+    "url": "/Dev-Portfolio/static/media/Weather-Wheel-Demo.74a92a2e.gif"
+  },
+  {
+    "revision": "1c375ceabb4437e60f25ce7fc5fca8d1",
+    "url": "/Dev-Portfolio/static/media/portfolio-demo.1c375cea.gif"
+  },
+  {
+    "revision": "a8db822cd6b5f1bddb9cf1d0c18d08a6",
+    "url": "/Dev-Portfolio/static/media/me.a8db822c.png"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/Dev-Portfolio/static/media/mongo.e5eee315.png"
+  },
+  {
+    "revision": "4bad5f07ce38e365019a6afe3db498e9",
+    "url": "/Dev-Portfolio/static/media/node.4bad5f07.png"
+  },
+  {
+    "revision": "2452f53338fcbb9b15936f609a1d3840",
+    "url": "/Dev-Portfolio/static/media/gql.2452f533.png"
+  },
+  {
+    "revision": "5e37495b93f1991ef91e1d228e87b200",
+    "url": "/Dev-Portfolio/static/media/html.5e37495b.png"
+  },
+  {
+    "revision": "cf6f124de378e7228b78de6b83dbc174",
+    "url": "/Dev-Portfolio/static/media/java.cf6f124d.png"
+  },
+  {
+    "revision": "2306183bbfe3a68f9fe63aa839afd292",
+    "url": "/Dev-Portfolio/static/media/Nodes.2306183b.svg"
+  },
+  {
+    "revision": "c89a5168fd116d12e8d750fb37b80ba1",
+    "url": "/Dev-Portfolio/index.html"
+  }
+];
